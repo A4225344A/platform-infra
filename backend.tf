b976d652e277ea2cf5bee99b51400fbfdf2d5722
Smart Dev Team <dev@smartdev.local>
2026-08-15 09:28:43 +0800

    W1: remove stale backend.tf pointing to deleted bucket (restarting bootstrap)

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "platform-tfstate-029099141993"
-    key            = "platform/terraform.tfstate"
-    region         = "ap-northeast-1"
-    dynamodb_table = "platform-tflock"
-    encrypt        = true
-  }
-}
