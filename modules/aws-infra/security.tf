0f2abf5a78a46a89d23bc8eaef8ea0b7cf568fdf
Smart Dev Team <dev@smartdev.local>
2026-09-02 21:43:45 +0800

    Use HTTP-only CloudFront origin ingress

--- modules/aws-infra/security.tf
+++ modules/aws-infra/security.tf
@@ -16,14 +16,6 @@ resource "aws_vpc_security_group_ingress_rule" "http" {
   prefix_list_id    = data.aws_ec2_managed_prefix_list.cloudfront_origin_facing.id
   description       = "Allow HTTP only from CloudFront origin-facing edge locations"
 }
-resource "aws_vpc_security_group_ingress_rule" "https" {
-  security_group_id = aws_security_group.web.id
-  from_port         = 443
-  to_port           = 443
-  ip_protocol       = "tcp"
-  prefix_list_id    = data.aws_ec2_managed_prefix_list.cloudfront_origin_facing.id
-  description       = "Allow HTTPS only from CloudFront origin-facing edge locations"
-}
 resource "aws_vpc_security_group_egress_rule" "all" {
   security_group_id = aws_security_group.web.id
   ip_protocol       = "-1"
